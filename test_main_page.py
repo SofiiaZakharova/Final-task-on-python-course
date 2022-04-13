e2ea4355b892857d707696f23e5da12e93e76fbd
Sofiia_Zakharova <Sofiia_Zakharova@epam.com>
2022-04-13 15:09:32 +0300

    Add task: группировка тестов и setup

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,35 @@
+import pytest
+
 from pages.basket_page import BasketPage
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.product_page import ProductPage
+import time
+
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time()) + "123"
+        link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_text_message_is_correct()
 
 
 def test_guest_can_go_to_login_page(browser):
