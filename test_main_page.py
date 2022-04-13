87e7481cc62c754043e8930f0eec9e82190eb302
Sofiia_Zakharova <Sofiia_Zakharova@epam.com>
2022-04-13 15:49:25 +0300

    Подготовка кода к ревью

--- test_main_page.py
+++ test_main_page.py
@@ -58,14 +58,7 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.should_be_message_about_empty_basket_is_present()
 
 
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-girl-who-played-with-non-fire_203/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page = BasketPage(browser, link)
-    page.should_be_empty_basket()
-    page.should_be_message_about_empty_basket_is_present()
+
 
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,47 @@
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 from pages.product_page import ProductPage
 import pytest
 
 
-#@pytest.mark.parametrize('number', [0, 1, 2, 3, 4, 5, 6,
-                                    #pytest.param(7, marks=pytest.mark.xfail(reason="fixing this bug right now")), 8, 9])
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page = LoginPage(browser, link)
+    page.should_be_login_form()
 
+
+@pytest.mark.need_review
+def test_user_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_be_text_message_is_correct()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-girl-who-played-with-non-fire_203/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page = BasketPage(browser, link)
+    page.should_be_empty_basket()
+    page.should_be_message_about_empty_basket_is_present()
+
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_be_text_message_is_correct()
 
 
 @pytest.mark.skip
@@ -15,9 +51,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.open()
     page.add_to_basket()
     page.should_not_be_success_message()
-
-
-
 
 
 @pytest.mark.skip
@@ -34,13 +67,3 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    page = LoginPage(browser, link)
-    page.should_be_login_form()
-
